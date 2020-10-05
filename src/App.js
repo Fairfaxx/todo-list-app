baa4a00813a90d3459a5fccdaa4efe3b769846d4
Fede Lemaire <fede.lemaire@gmail.com>
2020-10-05 10:37:15 -0300

    App component structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoItem from './components/TodoItem';
 //bla
 function App() {
+  const initialValues = [
+    { id: 1, title: "Wake Up" },
+    { id: 2, title: "Make Breakfast" }
+  ];
+
+  const [items, setItems] = useState(initialValues);
+  const [item, setItem] = useState('');
+  const [editItem, setEditItem] = useState(false);
+  const [id, setId] = useState(uuidv4());
+
+  const handleChange = (e) => {
+    console.log('handle change')
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('handle submit');
+  };
+
+  const clearList = () => {
+    console.log('clear List');
+  };
+
+  const handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  }
+
+  const handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
+  }
+
   return (
-    <div >
-      <TodoInput />
-      <TodoList />
+    <div className='container'>
+      <div className="row">
+        <div className="col-10 col-md-8 mx-auto mt-5">
+          <h3 className='text-capitalize text-center'>Todo Input</h3>
+          <TodoInput
+            item={setItem}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            editItem={setEditItem}
+          />
+          <TodoList
+            items={setItems}
+            clearList={clearList}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        </div>
+      </div>
     </div>
   );
 }
